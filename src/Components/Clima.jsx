a06d4ba09276c8469b783f7add6894ea14f9065d
Javier Uncos <javiersrfc30@gmail.com>
2022-10-10 22:01:48 -0300

    instalamos sweetalert y agregamos validaciones al formulario

--- src/Components/Clima.jsx
+++ src/Components/Clima.jsx
@@ -1,30 +1,32 @@
 import React from "react";
-import { Col, Card, Button } from "react-bootstrap";
+import { Col, Card } from "react-bootstrap";
 import "./CardEstilo.css";
 
 const Clima = ({ item }) => {
   return (
-    <Col md={4} className="mx-auto mt-4">
-      <Card>
-        <Card.Body>
-          <div className="d-flex justify-content-between ">
-            <Card.Title className="my-2 ms-3 display">{item.name}</Card.Title>
-            <div className="circuloPais bg-success">
-              <h4>{item.sys.country}</h4>
+    <>
+      <Col md={4} className="mx-auto mt-4">
+        <Card>
+          <Card.Body>
+            <div className="d-flex justify-content-between ">
+              <Card.Title className="my-2 ms-3 display">{item.name}</Card.Title>
+              <div className="circuloPais bg-success">
+                <h4>{item.sys.country}</h4>
+              </div>
             </div>
-          </div>
-          <div className="mt-4">
-            <ul className="lista d-flex justify-content-center flex-column">
-              <li>Humedad: °{item.main.humidity}</li>
-              <li>Temperatura: °{item.main.temp}</li>
-              <li>Temperatura-max: °{item.main.temp_max}</li>
-              <li>Temperatura-min: °{item.main.temp_min}</li>
-              <li>Temperatura-min: °{item.sys.country}</li>
-            </ul>
-          </div>
-        </Card.Body>
-      </Card>
-    </Col>
+            <div className="mt-4">
+              <ul className="lista d-flex justify-content-center flex-column">
+                <li>Humedad: °{item.main.humidity}</li>
+                <li>Temperatura: °{item.main.temp}</li>
+                <li>Temperatura-max: °{item.main.temp_max}</li>
+                <li>Temperatura-min: °{item.main.temp_min}</li>
+                <li>Temperatura-min: °{item.sys.country}</li>
+              </ul>
+            </div>
+          </Card.Body>
+        </Card>
+      </Col>
+    </>
   );
 };
 
--- src/Components/Formulario.jsx
+++ src/Components/Formulario.jsx
@@ -2,11 +2,23 @@ import React, { useState } from "react";
 import { Form, Button } from "react-bootstrap";
 import ListaDelclima from "./ListaDelclima";
 import "./Formulario.css";
+import Swal from "sweetalert2";
 
 const Formulario = () => {
   const [ubicacion, setUbicacion] = useState("");
   const [pais, setPais] = useState("");
   const [arregloClima, setArregloClima] = useState([]);
+
+
+  const mostrarAlerta =()=>{
+    Swal.fire({
+      title: 'Error!',
+      text: 'No se pudo encontrar un resultado',
+      icon: 'error',
+      confirmButtonText: 'Cerrar'
+    })
+  }
+  
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -15,14 +27,18 @@ const Formulario = () => {
         `https://api.openweathermap.org/data/2.5/weather?q=${ubicacion},${pais}&appid=43d81ec26d29e1c2ce80bbdcd7bc714f&units=metric`
       );
       const dato = await respuesta.json();
+      if (dato.sys.country === "") {
+        console.log("error");
+      }
       setArregloClima([dato]);
-     
+
+      dato === dato.error ? alert("error") : console.log("error");
     } catch (error) {
-      console.log(error)
+       mostrarAlerta()
     }
 
-    setUbicacion("")
-    setPais("")
+    setUbicacion("");
+    setPais("");
   };
 
   return (
@@ -32,10 +48,14 @@ const Formulario = () => {
         <hr />
       </div>
       <div className="d-flex justify-content-center">
-        <Form className=" my-3 bg-success shadow text-light" onSubmit={handleSubmit}>
+        <Form
+          className=" my-3 bg-success shadow text-light"
+          onSubmit={handleSubmit}
+        >
           <Form.Group className="mb-3 w-75 mx-auto" controlId="formBasicEmail">
             <Form.Label>Ubicacion</Form.Label>
             <Form.Control
+              required
               type="text"
               placeholder="ej Tucuman"
               onChange={(e) => {
@@ -44,19 +64,24 @@ const Formulario = () => {
               value={ubicacion}
             />
           </Form.Group>
-          <Form.Group className="mb-3 w-75 mx-auto" controlId="formBasicPassword">
+          <Form.Group
+            className="mb-3 w-75 mx-auto"
+            controlId="formBasicPassword"
+          >
             <Form.Label>Pais</Form.Label>
             <Form.Control
+              className="form-control"
               type="text"
               placeholder="ej Argentina"
               onChange={(e) => {
                 setPais(e.target.value);
               }}
               value={pais}
+              required
             />
           </Form.Group>
           <div className="d-flex justify-content-center">
-            <Button variant="warning" type="submit" onClick={handleSubmit}>
+            <Button variant="warning" type="submit">
               Consultar
             </Button>
           </div>
